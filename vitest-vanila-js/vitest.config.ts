e35793ca05d00859e2d9c97382bd5d8c8f6d8fa2
Javad Yousefi <javadyousefi9000@gmail.com>
2024-02-25 17:43:06 +0330

    in soruce testing

--- vitest-vanila-js/vitest.config.ts
+++ vitest-vanila-js/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    includeSource: ["./**/*.{js,ts}"],
+
     coverage: {
       provider: "v8", // or 'v8'
       reporter: ["text", "json", "html"],
